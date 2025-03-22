d9be7d9a106e722ce5ad0aa35b186edf61c943d0
Semih Esentürk <semihesenturk@outlook.com>
2025-03-22 21:40:07 +0300

    Db Connection and DI registers has added to Api Project
    Db Connection and DI registers has added to Api Project

--- src/LogiCommerce.API/Program.cs
+++ src/LogiCommerce.API/Program.cs
@@ -1,6 +1,19 @@
+using LogiCommerce.Domain.AggregateModels.ProductAggregate;
+using LogiCommerce.Domain.Generics;
+using LogiCommerce.Infrastructure.EFCore;
+using LogiCommerce.Infrastructure.EFCore.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<LogiCommerceDbContext>(options =>
+    options.UseInMemoryDatabase("LogiCommerceDb"));
+
+//DI Injections
+builder.Services.AddScoped(typeof(IRepository<>), typeof(RepositoryBase<>));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
