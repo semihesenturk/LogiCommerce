10bd9546caa89b1fc543556b9139301b42f2053c
Semih Esentürk <semihesenturk@outlook.com>
2025-03-23 13:57:24 +0300

    Filter specification changes
    Filter specification changes

--- src/LogiCommerce.Application/Product/Queries/GetProductsByKeywordOrMinMaxStock/GetProductsQuery.cs
+++ src/LogiCommerce.Application/Product/Queries/GetProductsByKeywordOrMinMaxStock/GetProductsQuery.cs
@@ -3,16 +3,10 @@ using MediatR;
 
 namespace LogiCommerce.Application.Product.Queries.GetProductsByKeywordOrMinMaxStock;
 
-public class GetProductsQuery : IRequest<BaseServiceResponse<List<GetProductsByKeywordQueryDto>>>
+public class GetProductsQuery(string? keyword, int? minStock, int? maxStock)
+    : IRequest<BaseServiceResponse<List<GetProductsByKeywordQueryDto>>>
 {
-    public string? Keyword { get; set; }
-    public int? MinStock { get; set; }
-    public int? MaxStock { get; set; }
-
-    public GetProductsQuery(string? keyword, int? minStock, int? maxStock)
-    {
-        Keyword = keyword;
-        MinStock = minStock;
-        MaxStock = maxStock;
-    }
+    public string? Keyword { get; set; } = keyword;
+    public int? MinStock { get; set; } = minStock;
+    public int? MaxStock { get; set; } = maxStock;
 }
--- src/LogiCommerce.Domain/AggregateModels/ProductAggregate/Specification/ProductFilterSpecification.cs
+++ src/LogiCommerce.Domain/AggregateModels/ProductAggregate/Specification/ProductFilterSpecification.cs
@@ -6,26 +6,21 @@ public class ProductFilterSpecification : Specification<Product>
 {
     public ProductFilterSpecification(string keyword, int? minStock, int? maxStock)
     {
-        // Category'i include ederek ilişkili verileri getiriyoruz
         Query.Include(p => p.Category);
-
-        // Eğer keyword parametresi varsa, filtreleme yapıyoruz
+        
         if (!string.IsNullOrEmpty(keyword))
         {
-            // Keyword ile ilgili Title, Description ve Category.Name üzerinde büyük/küçük harf duyarsız arama yapıyoruz
             Query.Where(p =>
                 p.Title.ToLower().Contains(keyword.ToLower()) ||
                 p.Description.ToLower().Contains(keyword.ToLower()) ||
                 p.Category.Name.ToLower().Contains(keyword.ToLower()));
         }
-
-        // Eğer minStock parametresi varsa, stok miktarını filtreliyoruz
+        
         if (minStock.HasValue)
         {
             Query.Where(p => p.StockQuantity >= minStock.Value);
         }
-
-        // Eğer maxStock parametresi varsa, stok miktarını filtreliyoruz
+        
         if (maxStock.HasValue)
         {
             Query.Where(p => p.StockQuantity <= maxStock.Value);
